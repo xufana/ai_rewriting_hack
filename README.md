36b0539b49d050e9d60f68cb5f7aaed24c874889
xufana <xufana@icloud.com>
2024-06-23 07:39:46 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ai_rewriting_hack
+
+## How to run:
+
+`pip install requirements.txt`
+
+`export AI_TOKEN="your_token"`
+
+`python main.py`
+or
+`python main.py --input_file=my_input.txt --output_file=my_output.txt`
---
+++ utils.py
@@ -0,0 +1,41 @@
+import re
+
+def clean_markdown(text):
+    # Remove Markdown link and image syntax
+    text = re.sub(r'!\[.*?\]\(.*?\)', '', text)  # Remove images
+    text = re.sub(r'\[.*?\]\(.*?\)', '', text)   # Remove links
+    
+    # Remove inline code
+    text = re.sub(r'`.*?`', '', text)
+    
+    # Remove bold and italics
+    text = re.sub(r'\*\*.*?\*\*', '', text)  # Remove bold **
+    text = re.sub(r'\*.*?\*', '', text)      # Remove italics *
+    text = re.sub(r'__.*?__', '', text)      # Remove bold __
+    text = re.sub(r'_.*?_', '', text)        # Remove italics _
+    
+    # Remove strikethrough
+    text = re.sub(r'~~.*?~~', '', text)
+    
+    # Remove headers
+    text = re.sub(r'\#{1,6}\s*', '', text)
+    
+    # Remove blockquotes
+    text = re.sub(r'\>\s*', '', text)
+    
+    # Remove unordered list markers
+    text = re.sub(r'[\*\-\+]\s+', '', text)
+    
+    # Remove ordered list markers
+    text = re.sub(r'\d+\.\s+', '', text)
+    
+    # Remove horizontal rules
+    text = re.sub(r'\-{3,}', '', text)
+    
+    # Remove remaining special characters used in markdown
+    text = re.sub(r'\\.', '', text)  # Remove escaped characters like \*
+    
+    # Remove extra whitespace
+    text = re.sub(r'\s{2,}', ' ', text).strip()
+    
+    return text
