7d054a300d470336a6737fd2a57002e4a8c831d7
Semerova <semenova@MacBook-Pro-2.local>
2024-06-23 10:22:44 +0300

    docstrings + isort + black

--- main.py
+++ main.py
@@ -1,70 +1,106 @@
-import openai
 import os
-import sys
-import re
-import fire
 import random
+import re
+import sys
+
+import fire
+import openai
 import pandas as pd
 
+from typing import List
+
+from langchain.docstore.document import Document
+from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain_experimental.text_splitter import SemanticChunker
 from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_qdrant import Qdrant
-from langchain.docstore.document import Document
-from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
 
 from utils import clean_markdown
 
 
-def contains_markdown(text):
+def contains_markdown(text: str) -> bool:
+    """
+    Checking the text contains markdowns
+
+    Parameters:
+        text (str) -- input text
+
+    Output:
+        (bool) -- result of checking, True -- contains, False -- not
+    """
     # Define a list of common Markdown patterns
     markdown_patterns = [
-        r'\*\*(.*?)\*\*',        # Bold (**bold**)
-        r'\*(.*?)\*',            # Italics (*italics*)
-        r'\_\_(.*?)\_\_',        # Bold (__bold__)
-        r'\_(.*?)\_',            # Italics (_italics_)
-        r'\~\~(.*?)\~\~',        # Strikethrough (~~strikethrough~~)
-        r'\`(.*?)\`',            # Inline code (`code`)
-        r'\#\s.*',               # Headers (# Header)
-        r'\>\s.*',               # Blockquotes (> Blockquote)
-        r'\[(.*?)\]\((.*?)\)',   # Links [text](url)
-        r'\!\[(.*?)\]\((.*?)\)', # Images ![alt text](url)
-        r'\n\s*\n',              # Paragraphs (blank line)
-        r'\n\d+\.\s',            # Ordered lists (1. item)
-        r'\n\-\s',               # Unordered lists (- item)
+        r"\*\*(.*?)\*\*",           # Bold (**bold**)
+        r"\*(.*?)\*",               # Italics (*italics*)
+        r"\_\_(.*?)\_\_",           # Bold (__bold__)
+        r"\_(.*?)\_",               # Italics (_italics_)
+        r"\~\~(.*?)\~\~",           # Strikethrough (~~strikethrough~~)
+        r"\`(.*?)\`",               # Inline code (`code`)
+        r"\#\s.*",                  # Headers (# Header)
+        r"\>\s.*",                  # Blockquotes (> Blockquote)
+        r"\[(.*?)\]\((.*?)\)",      # Links [text](url)
+        r"\!\[(.*?)\]\((.*?)\)",    # Images ![alt text](url)
+        r"\n\s*\n",                 # Paragraphs (blank line)
+        r"\n\d+\.\s",               # Ordered lists (1. item)
+        r"\n\-\s",                  # Unordered lists (- item)
     ]
-    
+
     for pattern in markdown_patterns:
         if re.search(pattern, text):
             return True
     return False
 
-def contains_russian(text):
+
+def contains_russian(text: str) -> bool:
+    """
+    Checking text contains russian characters
+
+    Parameters:
+        text (str) -- input text
+
+    Output:
+        (bool) -- result of checking, True -- contains, False -- not
+    """
     # Define a regular expression pattern to match any Russian character
-    russian_pattern = r'[а-яА-ЯёЁ]'
-    
+    russian_pattern = r"[а-яА-ЯёЁ]"
+
     # Search for the pattern in the text
     if re.search(russian_pattern, text):
         return True
     else:
         return False
 
+
 def extract_number_from_string(s: str) -> int:
     """
-    Extract a number from a string
-    """
+    Extracting a number from a string
 
-    pattern = r'\d+'
+    Parameters:
+        s (str) -- input string
+
+    Output:
+        (int) -- result of extracting, return number if contains, 1 otherwise
+    """
+    pattern = r"\d+"
     match_ = re.search(pattern, s)
+
     if match_:
         return int(match_.group())
     else:
-        return 1 # if LLM could not decide whether it is a generated text, rewrite it anyway
+        return 1  # if LLM could not decide whether it is a generated text, rewrite it anyway
 
-def check_ai(text, llm="gpt-3.5-turbo-0125") -> bool:
-    """
-    A classifier to check whether the text is AI-generated or not.
-    """
 
+def check_ai(text: str, llm: str = "gpt-3.5-turbo-0125") -> bool:
+    """
+    A classifier to check whether the text is AI-generated or not
+
+    Parameters:
+        text (str) -- input text
+        llm (str) -- model
+
+    Output:
+        (bool) -- result of classifier, True -- if the text is written by an AI, False -- otherwise
+    """
     system = """You are a helpful assistant that checks if the text was written by AI, not by a human.
     Answer only 0, if the text is written by a human.
     Answer only 1, if the text is written by an AI.
@@ -74,7 +110,7 @@ def check_ai(text, llm="gpt-3.5-turbo-0125") -> bool:
         model=llm,
         messages=[
             {"role": "system", "content": system},
-            {"role": "user", "content": f"Text: {text}"}
+            {"role": "user", "content": f"Text: {text}"},
         ],
         max_tokens=5,
         # logprobs=1,
@@ -84,11 +120,17 @@ def check_ai(text, llm="gpt-3.5-turbo-0125") -> bool:
     return text_class == 1
 
 
-def paraphrase_text(text,  model="gpt-3.5-turbo"):
+def paraphrase_text(text: str, model: str = "gpt-3.5-turbo") -> str:
     """
-    Trying to rewrite a text.
-    """
+    Trying to rewrite a text
 
+    Parameters:
+        text (str) -- input text
+        model (str) -- model
+
+    Output:
+        paraphrased_text (str) -- paraphrased text
+    """
     system = """You are a dedicated writer that paraphrases texts, while keeping the original style and answer using the same language.
     Try not to fantasize and avoid list structures in data presentation, use natural language. Do not use bullet-point structure or lists. Do not use complex vocabulary
     Do not use many linking words, do not repeat arguments, but use various sentence structures. Instead of using advanced words talk to me like I am not a native speaker.
@@ -98,24 +140,72 @@ def paraphrase_text(text,  model="gpt-3.5-turbo"):
         model=model,
         messages=[
             {"role": "system", "content": system},
-            {"role": "user", "content": system + '\n' + text}
+            {"role": "user", "content": system + "\n" + text},
         ],
         temperature=0.85,
         top_p=0.85,
         presence_penalty=-1,
     )
+
     paraphrased_text = response.choices[0].message.content.strip()
+
     return paraphrased_text
 
-def main(input_file='input.txt', output_file='output.txt'):
-    openai.api_key = os.getenv('AI_TOKEN')
+
+def get_context(text: str, russian: bool, relevant: List[Document]) -> str:
+    """
+    Getting context
+
+    Parameters:
+        text (str) -- input text
+        russian (bool) -- flag if text contains russian characters
+        relevant (List[Document]) -- retrieve documents
+
+    Output:
+        context (str) -- result with context
+    """
+    if russian:
+        context = "\n".join(
+            [
+                f"Перефразируй данный текст: {relevant_doc.page_content}\nИтог: {relevant_doc.metadata['original']}"
+                for relevant_doc in relevant
+            ]
+        )
+        context = context + f"\nПерефразируй данный текст: {text}\nИтог: "
+    else:
+        context = "\n".join(
+            [
+                f"Paraphrase the following text: {relevant_doc.page_content}\nResult: {relevant_doc.metadata['original']}"
+                for relevant_doc in relevant
+            ]
+        )
+        context = context + f"\nParaphrase the following text: {text}\nResult: "
+
+    return context
+
+
+def main(input_file: str = "input.txt", output_file: str = "output.txt") -> None:
+    """
+    Main function with total pipeline:
+        -- checking text is written by an AI
+        -- if yes, we rewhrite
+        -- saving result in output file
+
+    Parameters:
+        input_file (str) -- path to input file
+        output_file (str) -- path to output file
+
+    Output:
+        None
+    """
+    openai.api_key = os.getenv("AI_TOKEN")
 
     if openai.api_key is None:
         print("Error: AI_TOKEN environment variable is not set.")
         sys.exit(1)
 
     try:
-        with open(input_file, 'r', encoding='utf-8') as file:
+        with open(input_file, "r", encoding="utf-8") as file:
             text = file.read()
     except FileNotFoundError:
         print(f"Error: The file {input_file} was not found.")
@@ -123,41 +213,43 @@ def main(input_file='input.txt', output_file='output.txt'):
 
     russian = contains_russian(text)
 
-    db_docs = pd.read_csv("qdrant_data/texts.csv")
-    db_docs = [Document(page_content=row.paraphrased, metadata={"original": row.text}) for _, row in db_docs.iterrows()]
+    db_docs = pd.read_csv("./qdrant_data/texts.csv")
+    db_docs = [
+        Document(page_content=row.paraphrased, metadata={"original": row.text})
+        for _, row in db_docs.iterrows()
+    ]
 
     embeddings = HuggingFaceEmbeddings(
         model_name="DeepPavlov/distilrubert-tiny-cased-conversational-v1"
-        )
+    )
     qdrant = Qdrant.from_documents(
         db_docs,
         embeddings,
         location=":memory:",
         collection_name="my_documents",
-        )
-        
+    )
+
     qdrant_retriever = qdrant.as_retriever(search_kwargs={"k": 10}, verbose=True)
 
     print("checking AI-gen text...")
-    
+
     if contains_markdown(text):
         # if there is a markdown, we rewrite text completely
+        # print("Text contains markdowns")
         relevant = qdrant_retriever.get_relevant_documents(text)
+        context = get_context(text, russian, relevant)
 
-        if russian:
-            context = '\n'.join([f"Перефразируй данный текст: {relevant_doc.page_content}\nИтог: {relevant_doc.metadata['original']}" for relevant_doc in relevant])
-            context = context + f"\nПерефразируй данный текст: {text}\nИтог: "
-        else:
-            context = '\n'.join([f"Paraphrase the following text: {relevant_doc.page_content}\nResult: {relevant_doc.metadata['original']}" for relevant_doc in relevant])
-            context = context + f"\nParaphrase the following text: {text}\nResult: "
-        
         text = paraphrase_text(context)
     elif check_ai(text):
-        # if there is no markdown we use classifier and rewrite only some parts of the text
-        text_splitter = SemanticChunker(OpenAIEmbeddings(openai_api_key=os.getenv('AI_TOKEN')))
+        # if there is no markdown, we use classifier and rewrite only some parts of the text
+        # print("Text is written by an AI")
+        text_splitter = SemanticChunker(
+            OpenAIEmbeddings(openai_api_key=os.getenv("AI_TOKEN"))
+        )
         docs = text_splitter.create_documents([text.strip()])
-        if (len(docs) == 0):
-            docs = text.strip().split('\n')
+
+        if len(docs) == 0:
+            docs = text.strip().split("\n")
 
         try:
             sample_ids = random.sample(range(0, len(docs) - 1), 5)
@@ -168,27 +260,23 @@ def main(input_file='input.txt', output_file='output.txt'):
         for i, doc in enumerate(docs):
             if i in sample_ids:
                 relevant = qdrant_retriever.get_relevant_documents(doc.page_content)
+                context = get_context(doc.page_content, russian, relevant)
 
-                if russian:
-                    context = '\n'.join([f"Перефразируй данный текст: {relevant_doc.page_content}\nИтог: {relevant_doc.metadata['original']}" for relevant_doc in relevant])
-                    context = context + f"\nПерефразируй данный текст: {doc.page_content}\nИтог: "
-                else:
-                    context = '\n'.join([f"Paraphrase the following text: {relevant_doc.page_content}\nResult: {relevant_doc.metadata['original']}" for relevant_doc in relevant])
-                    context = context + f"\nParaphrase the following text: {doc.page_content}\nResult: "
                 chunk = paraphrase_text(context)
             else:
                 chunk = doc.page_content
             result.append(chunk)
-        
-        text = '\n'.join(result)
+
+        text = "\n".join(result)
 
     if contains_markdown(text):
         text = clean_markdown(text)
 
-    with open(output_file, 'w', encoding='utf-8') as file:
+    with open(output_file, "w", encoding="utf-8") as file:
         file.write(text)
 
     print(f"Paraphrased text has been saved to {output_file}")
+
 
 if __name__ == "__main__":
     fire.Fire(main)
--- utils.py
+++ utils.py
@@ -1,41 +1,51 @@
 import re
 
-def clean_markdown(text):
+
+def clean_markdown(text: str) -> str:
+    """
+    Removing markdowns
+
+    Parameters:
+        text (str) -- input text
+
+    Output:
+        text (str) -- preprocessed text
+    """
     # Remove Markdown link and image syntax
-    text = re.sub(r'!\[.*?\]\(.*?\)', '', text)  # Remove images
-    text = re.sub(r'\[.*?\]\(.*?\)', '', text)   # Remove links
-    
+    text = re.sub(r"!\[.*?\]\(.*?\)", "", text)  # Remove images
+    text = re.sub(r"\[.*?\]\(.*?\)", "", text)  # Remove links
+
     # Remove inline code
-    text = re.sub(r'`.*?`', '', text)
-    
+    text = re.sub(r"`.*?`", "", text)
+
     # Remove bold and italics
-    text = re.sub(r'\*\*.*?\*\*', '', text)  # Remove bold **
-    text = re.sub(r'\*.*?\*', '', text)      # Remove italics *
-    text = re.sub(r'__.*?__', '', text)      # Remove bold __
-    text = re.sub(r'_.*?_', '', text)        # Remove italics _
-    
+    text = re.sub(r"\*\*.*?\*\*", "", text)  # Remove bold **
+    text = re.sub(r"\*.*?\*", "", text)  # Remove italics *
+    text = re.sub(r"__.*?__", "", text)  # Remove bold __
+    text = re.sub(r"_.*?_", "", text)  # Remove italics _
+
     # Remove strikethrough
-    text = re.sub(r'~~.*?~~', '', text)
-    
+    text = re.sub(r"~~.*?~~", "", text)
+
     # Remove headers
-    text = re.sub(r'\#{1,6}\s*', '', text)
-    
+    text = re.sub(r"\#{1,6}\s*", "", text)
+
     # Remove blockquotes
-    text = re.sub(r'\>\s*', '', text)
-    
+    text = re.sub(r"\>\s*", "", text)
+
     # Remove unordered list markers
-    text = re.sub(r'[\*\-\+]\s+', '', text)
-    
+    text = re.sub(r"[\*\-\+]\s+", "", text)
+
     # Remove ordered list markers
-    text = re.sub(r'\d+\.\s+', '', text)
-    
+    text = re.sub(r"\d+\.\s+", "", text)
+
     # Remove horizontal rules
-    text = re.sub(r'\-{3,}', '', text)
-    
+    text = re.sub(r"\-{3,}", "", text)
+
     # Remove remaining special characters used in markdown
-    text = re.sub(r'\\.', '', text)  # Remove escaped characters like \*
-    
+    text = re.sub(r"\\.", "", text)  # Remove escaped characters like \*
+
     # Remove extra whitespace
-    text = re.sub(r'\s{2,}', ' ', text).strip()
-    
+    text = re.sub(r"\s{2,}", " ", text).strip()
+
     return text
